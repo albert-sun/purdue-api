418b3cd0b21efcd9c30e90302e4cbf112d87a2bc
albert-sun <sun831@purdue.edu>
2020-08-27 21:33:56 -0400

    removed config because unused

--- config.go
+++
@@ -1,6 +0,0 @@
-package purdue_api
-
-// Config contains global settings for use between hooks such as concurrency, etc.
-type Config struct {
-	Concurrent int `json:"concurrent"` // number of concurrent requests
-}
